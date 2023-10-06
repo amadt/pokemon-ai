719f05f82f75c9d1d40eb452b06d2e1fb386e1af
Adam Thomson <athomson@adobe.com>
2023-10-06 15:03:27 -0600

    new pokeball

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
+import logo from './pokeball.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is my Pokemon site
-        </p>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2 className="App-header">
+        William's Pokemon Generator
+      </h2>
+      <h3>Tech Fair 2023</h3>
     </div>
   );
 }
