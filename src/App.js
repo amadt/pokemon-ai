856733994ee6b88870238264864ff9287e0c8199
Adam Thomson <amadt5@gmail.com>
2023-10-07 11:03:04 -0600

    added dummy code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import logo from './pokeball.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [animalInput, setAnimalInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ animal: animalInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setAnimalInput("");
+      window.alert(data.result)
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
@@ -9,6 +39,20 @@ function App() {
         William's Pokemon Generator
       </h2>
       <h3>Tech Fair 2023</h3>
+
+      <h3>Name my pet</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="animal"
+            placeholder="Enter an animal"
+            value={animalInput}
+            onChange={(e) => setAnimalInput(e.target.value)}
+          />
+          <input type="submit" value="Generate names" />
+        </form>
+
+
     </div>
   );
 }
