6cbff8d4b83e4379119ae8a06402269a72dd029f
Adam Thomson <amadt5@gmail.com>
2023-10-15 14:49:46 -0600

    work on ai connect

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import { useState } from "react";
 import generate from './api/generate';
 
 function App() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [pokemonType, setPokemonType] = useState("Normal");
+  const [pokemonWord, setPokemonWord] = useState("");
+  const [pokemonDesc, setPokemonDesc] = useState("");
+
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
     try {
-      const data = await generate(animalInput);
-      console.log(data);
+      console.log(pokemonDesc, pokemonType, pokemonWord);
+      const data = await generate(pokemonType, pokemonWord, pokemonDesc);
 
       setResult(data[0]?.message?.content);
-      setAnimalInput("");
+      
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -31,20 +34,75 @@ function App() {
       </h2>
       <h3>Tech Fair 2023</h3>
 
-      <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
+      <form 
+        onSubmit={onSubmit}
+        className="pokemonForm"
+      >
+        <div
+          className="formRow"
+        >
+          Pokemon Type
+        
+          <select
+            value={pokemonType}
+            onChange={(e) => setPokemonType(e.target.value) }
+          >
+            <option>Normal</option>
+            <option>Fire</option>
+            <option>Water</option>
+            <option>Grass</option>
+            <option>Bug</option>
+            <option>Dark</option>
+            <option>Dragon</option>
+            <option>Electric</option>
+            <option>Fairy</option>
+            <option>Fighting</option>
+            <option>Flying</option>
+            <option>Ghost</option>
+            <option>Ground</option>
+            <option>Ice</option>
+            <option>Poison</option>
+            <option>Psychic</option>
+            <option>Rock</option>
+            <option>Steel</option>
+            
+          </select>
+
+        </div>
+        <div
+          className="formRow"
+        >
+          One word description
+        
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            placeholder="Enter a word"
+            value={pokemonWord}
+            onChange={(e) => setPokemonWord(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
-        </form>
 
-        <div>{result}</div>
+        </div>
+        <div
+          className="formRow"
+        >
+          Describe
+        
+          <input
+            type="text"
+            name="animal"
+            placeholder="Enter a description"
+            value={pokemonDesc}
+            onChange={(e) => setPokemonDesc(e.target.value)}
+          />
 
+        </div>
+
+        <input type="submit" value="Generate Pokemon" />
+      </form>
+
+      <div>{result}</div>
+        
     </div>
   );
 }
--- src/api/generate.js
+++ src/api/generate.js
@@ -7,24 +7,30 @@ const openai = new OpenAI({
   dangerouslyAllowBrowser: true 
 });
 
-async function generate(animal) {
+async function generate(type, word, description) {
   if (!openai.apiKey) {
     throw new Error("OpenAI API key not configured, please follow instructions in README.md");
   }
 
-  if (animal.trim().length === 0) {
-    throw new Error("Please enter a valid animal");
+  if (word.trim().length === 0) {
+    throw new Error("Please enter a valid word");
+  }
+  if (description.trim().length === 0) {
+    throw new Error("Please enter a valid description");
   }
 
   const completion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [
-      { role: 'system', content: 'Suggest three names for an animal that is a superhero.' }, 
-      { role: 'user', content: 'Animal: Cat' },
-      { role: 'assistant', content: 'Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline' },
-      { role: 'user', content: 'Animal: Dog' },
-      { role: 'assistant', content: 'Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot' },
-      { role: 'user', content: `Animal: ${animal}` },
+      { role: 'system', content: `
+        You are a game developer creating new Pokemon. 
+        We will provide the type of pokemon and a brief description. 
+        You will generate a name and a description for the new pokemon.
+        Provide a HP value, height (in feet and inches) and weight (in pounds).
+        The description should be no longer than 120 charecters.
+        chose one or two moves already existing `
+      }, 
+      { role: 'user', content: `This is a ${word} Pokemon with a type of ${type} and the following description: ${description}. ` },
     ],
     temperature: 0.6,
   });
